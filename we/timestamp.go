2fde3dfb8490ace0efef9d9747658bc9acba751d
Kevin O’Neill <kevin@oneill.id.au>
2022-07-20 14:38:36 +1000

    feat: timestamp utilities
    methods to convert timestamps to string and time

--- we/timestamp.go
+++ we/timestamp.go
@@ -7,5 +7,13 @@ type Timestamp string
 const RFC3339Milli = "2006-01-02T15:04:05.999Z07:00"
 
 func TimestampFromTime(t time.Time) Timestamp {
-	return Timestamp(t.UTC().Format(RFC3339Milli))
+  return Timestamp(t.UTC().Format(RFC3339Milli))
+}
+
+func (t Timestamp) String() string {
+  return string(t)
+}
+
+func (t Timestamp) Time() (time.Time, error) {
+  return time.Parse(RFC3339Milli, t.String())
 }
